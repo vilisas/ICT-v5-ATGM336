5c31fbe15bd6642a5ee0f81dccd820be463596c0
Vilius LY3FF <vilisas@gmail.com>
2021-03-19 14:54:55 +0200

    local configuration file added

--- config.h
+++ config.h
@@ -2,15 +2,16 @@
  * config.h
  *
  *  Created on: Mar 18, 2021
- *      Author: vilisas
+ *      Author: LY3FF
  */
 
 #ifndef CONFIG_H_
 #define CONFIG_H_
+#include "local_config.h"
 
-
-#define CONFIG_CALLSIGN "N0CALL" // set your callsign here
-
+#ifndef CONFIG_CALLSIGN
+#define CONFIG_CALLSIGN "N0CALL" // set your callsign in local_config.h file
+#endif
 
 #define WSPR_CTC                5336   // CTC value for WSPR - 10672 @ 16Mhz //5336 @ 8Mhz //2668 @ 4Mhz //1334 @ 2Mhz //667 @ 1Mhz
 #define WSPR_TONE_SPACING       146    // 146 ~1.46 Hz
